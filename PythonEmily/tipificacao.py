564ab840aacae7d2d288de90352e147622870f71
basiqly <151444372+basiqly@users.noreply.github.com>
2025-07-14 17:17:13 -0300

    Upload Primeira Aula
    Exercises of the first python lesson 01

---
+++ PythonEmily/tipificacao.py
@@ -0,0 +1,26 @@
+""" Trabalhando com tipificações e variaveis """
+
+nome = "Emily"
+print(nome)
+print(nome)
+
+
+sobrenome = 'Damian Colman' #string(ou seja, tipo texto)
+idade = 18 #integer(numero inteiro)
+altura = 1.70 #float(valores decimais)
+bermuda = False #boolean(Sim/Não)
+
+print(nome + " " + sobrenome + " tem " + str(idade) + "anos")
+print(idade + 2)
+
+textoVariasLinhas = ''' 
+esse é um texto
+ de varias
+ linhas
+'''
+print(textoVariasLinhas)
+
+
+
+
+
